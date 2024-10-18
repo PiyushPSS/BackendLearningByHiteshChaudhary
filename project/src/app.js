9b63c6c33435ee2d683249ffc2f4c21a42f735e4
Piyush <piyushpratapsingh67@gmail.com>
2024-10-18 18:36:14 +0530

    adding user routes

--- project/src/app.js
+++ project/src/app.js
@@ -23,4 +23,13 @@ app.use(express.static("public"));
 
 app.use(cookieParser());
 
+//routes
+import userRouter from './routes/user.routes.js';
+
+//routes declartion. When we were doing with app.get() then we could have used the normal way of using router, that is: app.get('/addUser'). But since we are using a router from an external file, so we need to use a different way. And that way is: using app.use()
+
+app.use("/api/v1/users", userRouter);
+
+//The app will be made this way now: http://localhost:3000/api/v1/users/register
+
 export default app;
